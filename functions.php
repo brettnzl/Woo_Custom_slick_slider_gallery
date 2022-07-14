0153dc8d9d2ee76e4cf6f4982316fc8ce7dbac2a
Brett Ransley <Brettrans@gmail.com>
2022-07-14 14:50:08 +1200

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,67 +2,92 @@
 
 	add_action('nb_woocommerce_product_gallery', 'custom_gallery');
 	function custom_gallery() {
+		if (isset($_GET['brett'])) {
+			
+			// Enqueue Required scripts & Styles
+			// wp_enqueue_script( 'lightbox', 'https://cdn.jsdelivr.net/npm/bs5-lightbox@1.7.8/dist/index.bundle.min.js'); // Lightbox/modelbox
+    			wp_enqueue_script( 'slick-js', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js'); // Slick Slider
+			wp_enqueue_style( 'slick-css', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css'); // Slick Slider
+			
 
-		// Enqueue Required scripts & Styles
-		wp_enqueue_script( 'lightbox', 'https://cdn.jsdelivr.net/npm/bs5-lightbox@1.7.8/dist/index.bundle.min.js'); // Lightbox/modelbox
-		wp_enqueue_script( 'slick-js', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js'); // Slick Slider
-		wp_enqueue_style( 'slick-css', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css'); // Slick Slider
+			echo "<div id='woo-gallery-nb'>";
+				echo "<div class='gallery-list'>";
+					global $product;
+					
+					// get main image + gallery images
+					$main_id = get_post_thumbnail_id( $product->get_ID() );
+					$gallery_ids = $product->get_gallery_image_ids();
 
+					// Get Variation images
+					$variations = $product->get_available_variations();
+					
+					// Merge the ID's
+					$all_ids = array();
+					$all_ids[] = $main_id;
+					$all_ids = array_merge($all_ids, $gallery_ids);
 
-		echo "<div id='woo-gallery-em'>";
-			echo "<div class='gallery-list'>";
-				global $product;
-
-				$attachment_ids = $product->get_gallery_image_ids();
-
-				foreach( $attachment_ids as $attachment_id ) {
-					$image_link = wp_get_attachment_url( $attachment_id, 'full' );
-					?>
-					<!-- <a href="<?php echo $image_link; ?>" data-lightbox="woo-gallery"> -->
-						<img src="<?php echo $image_link; ?>" alt="woo-gallery-image" class="w-100 img-fluid">
-					<!-- </a> -->
-					<?php 
-				}
-			echo "</div>";
-			echo "<div class='nav-list'>";
-				foreach( $attachment_ids as $attachment_id ) {
-					$image_link = wp_get_attachment_url( $attachment_id, 'thumbnail' );
-					?>
-					<img src="<?php echo $image_link; ?>" alt="woo-gallery-image" class="w-100 img-fluid">
-					<?php 
-				}
-			echo "</div>";
-		echo "</div>";
-		?>
-		<script>
-			jQuery(window).ready(function(){
-				jQuery('#woo-gallery-em .gallery-list').slick({
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					arrows: false,
-					fade: true,
-					asNavFor: '#woo-gallery-nb .nav-list'
-				});
-
-				jQuery('#woo-gallery-em .nav-list').slick({
-					slidesToShow: 5,
-					slidesToScroll: 1,
-					asNavFor: '#woo-gallery-nb .gallery-list',
-					dots: true,
-					centerMode: true,
-					focusOnSelect: true,
-					responsive: [{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 3,
-							slidesToScroll: 3,
-							infinite: true,
-							dots: true
+					if (!empty($variations)) {
+						foreach ( $variations as $variation ) {
+							$all_ids[] = $variation['image_id'];
 						}
-					}]
-				});
-			})				
-		</script>
-	<?php 
+					}
+					
+					// print_r($all_ids);
+					if (!empty($all_ids)) {
+						foreach( $all_ids as $attachment_id ) {
+							$image_link = wp_get_attachment_url( $attachment_id, 'full' );
+							?>
+							<!-- <a href="<?php echo $image_link; ?>" data-lightbox="woo-gallery"> -->
+								<div class="gallery-image"><img src="<?php echo $image_link; ?>" alt="woo-gallery-image" class="w-100 img-fluid"></div>
+							<!-- </a> -->
+							<?php 
+						}
+					}
+				echo "</div>";
+				echo "<div class='nav-list'>";
+					if (!empty($all_ids)) {
+						foreach( $all_ids as $attachment_id ) {
+							$image_link = wp_get_attachment_url( $attachment_id, 'thumbnail' );
+							?>
+							<div class="thumbnail-image"><img src="<?php echo $image_link; ?>" alt="woo-gallery-image" class="w-100 img-fluid"></div>
+							<?php 
+						}
+					}
+				echo "</div>";
+			echo "</div>";
+			?>
+			<script>
+				jQuery(window).ready(function(){
+					jQuery('#woo-gallery-nb .gallery-list').slick({
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						arrows: false,
+						fade: true,
+						asNavFor: '#woo-gallery-nb .nav-list'
+					});
+
+					jQuery('#woo-gallery-nb .nav-list').slick({
+						slidesToShow: 5,
+						slidesToScroll: 1,
+						asNavFor: '#woo-gallery-nb .gallery-list',
+						dots: true,
+						centerMode: true,
+						focusOnSelect: true,
+						responsive: [{
+							breakpoint: 1024,
+							settings: {
+								slidesToShow: 3,
+								slidesToScroll: 3,
+								infinite: true,
+								dots: true
+							}
+						}]
+					});
+				})				
+			</script>
+			<?php 
+
+		} else {
+			do_action( 'woocommerce_before_single_product_summary' );
+		}
 	}
-?>
